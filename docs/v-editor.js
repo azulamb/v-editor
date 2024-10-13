df9f72d828cd4b003d1a88b81c1d99b3c0ed58b2
azulamb <1127785+azulamb@users.noreply.github.com>
2024-10-13 21:56:10 +0900

    Fix.

--- docs/v-editor.js
+++ docs/v-editor.js
@@ -1,68 +1,53 @@
 ((script, init)=>{
+    const tagname = script.dataset['v-editor'] || 'v-editor';
+    if (customElements.get(tagname)) {
+        return;
+    }
     if (document.readyState !== 'loading') {
-        return init(script);
+        return init(script, tagname);
     }
     document.addEventListener('DOMContentLoaded', ()=>{
-        init(script);
+        init(script, tagname);
     });
-})(document.currentScript, (script)=>{
-    ((component, tagname = 'v-editor')=>{
-        if (customElements.get(tagname)) {
-            return;
+})(document.currentScript, (script, tagname)=>{
+    class DefaultVEditorEvent {
+        onPaste(editor, data, selection) {
+            const html = data.getData('text/html');
+            const parent = document.createElement('div');
+            if (html) {
+                parent.innerHTML = html;
+            } else {
+                parent.textContent = data.getData('text/plain');
+            }
+            editor.insertNodes(parent.childNodes, selection);
         }
-        customElements.define(tagname, component);
-    })(class extends HTMLElement {
-        editor;
+    }
+    customElements.define(tagname, class extends HTMLElement {
+        editorEvent = new DefaultVEditorEvent();
         headerCallbacks = [];
         constructor(){
             super();
             const style = document.createElement('style');
             style.innerHTML = [
-                ':host { background: #9393933d; width: 100%; display: block; }',
-                ':host > div { display: grid; width: 100%; height: 100%; }',
-                ':host > div > div { outline: none; overflow: auto; }'
+                ':host { background: #9393933d; width: 100%; display: block; overflow: auto; padding: 8px; border-radius: 4px; box-sizing: border-box; resize: both; outline: none; }'
             ].join('');
-            this.editor = document.createElement('div');
-            if (!this.readonly) {
-                this.editor.contentEditable = 'true';
-            }
-            this.editor.addEventListener('paste', (event)=>{
-                event.preventDefault();
-                event.stopPropagation();
-                console.log(event);
-                const selection = shadow.getSelection();
-                if (event.clipboardData && selection) {
-                    this.onPaste(event.clipboardData, selection);
-                }
+            this.addEventListener('paste', (event)=>{
+                this.onPasteEvent(event);
             });
-            const contents = document.createElement('div');
-            contents.appendChild(this.editor);
             const shadow = this.attachShadow({
                 mode: 'open'
             });
             shadow.appendChild(style);
-            shadow.appendChild(contents);
+            shadow.appendChild(document.createElement('slot'));
             this.changeHeader();
-        }
-        get readonly() {
-            return this.hasAttribute('readonly');
-        }
-        set readonly(value) {
-            if (value) {
-                this.editor.contentEditable = 'false';
-                this.setAttribute('readonly', '');
-            } else {
-                this.editor.contentEditable = 'true';
-                this.removeAttribute('readonly');
-            }
         }
         changeHeader() {
             for (const item of this.headerCallbacks){
                 item.target.removeEventListener('click', item.callback);
             }
             this.headerCallbacks = [];
-            const query = this.getAttribute('header');
-            if (!query) {
+            const selector = this.getAttribute('header');
+            if (!selector) {
                 return;
             }
             const callback = (event)=>{
@@ -71,8 +56,7 @@
                 const selection = document.getSelection();
                 console.log(selection);
             };
-            const elements = document.querySelectorAll(query);
-            for (const element of elements){
+            for (const element of document.querySelectorAll(selector)){
                 element.addEventListener('click', callback);
                 this.headerCallbacks.push({
                     target: element,
@@ -80,26 +64,29 @@
                 });
             }
         }
-        onPaste(data, selection) {
-            const html = data.getData('text/html');
-            const parent = document.createElement('div');
-            if (html) {
-                parent.innerHTML = html;
-                console.log(parent);
-            } else {
-                parent.textContent = data.getData('text/plain');
-            }
+        insertNodes(nodes, selection) {
             const range = selection.getRangeAt(0);
-            console.log(range);
             range.deleteContents();
-            for (const element of parent.childNodes){
+            for (const element of nodes){
                 range.insertNode(element);
             }
             selection.collapseToEnd();
         }
+        onPasteEvent(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            const data = event.clipboardData;
+            if (!data) {
+                return;
+            }
+            const selection = document.getSelection();
+            if (!selection) {
+                return;
+            }
+            this.editorEvent.onPaste(this, data, selection);
+        }
         static get observedAttributes() {
             return [
-                'readonly',
                 'header'
             ];
         }
@@ -108,11 +95,6 @@
                 return;
             }
             switch(name){
-                case 'readonly':
-                    {
-                        this.readonly = newValue !== null || newValue === 'true';
-                        return;
-                    }
                 case 'header':
                     {
                         this.changeHeader();
@@ -120,5 +102,5 @@
                     }
             }
         }
-    }, script.dataset.tagname);
+    });
 });
--- src/v-editor.ts
+++ src/v-editor.ts
@@ -1,103 +1,39 @@
 interface VEditorElement extends HTMLElement {
-  readonly: boolean;
+  editorEvent: VEditorEvent;
+  insertNodes(
+    nodes: NodeListOf<ChildNode> | ChildNode[],
+    selection: Selection,
+  ): void;
+}
+
+interface VEditorEvent {
+  onPaste(
+    editor: VEditorElement,
+    data: DataTransfer,
+    selection: Selection,
+  ): unknown;
 }
 
 ((script, init) => {
+  const tagname = script.dataset['v-editor'] || 'v-editor';
+  if (customElements.get(tagname)) {
+    return;
+  }
   if (document.readyState !== 'loading') {
-    return init(script);
+    return init(script, tagname);
   }
   document.addEventListener('DOMContentLoaded', () => {
-    init(script);
+    init(script, tagname);
   });
-})(<HTMLScriptElement> document.currentScript, (script: HTMLScriptElement) => {
-  ((component, tagname = 'v-editor') => {
-    if (customElements.get(tagname)) {
-      return;
-    }
-    customElements.define(tagname, component);
-  })(
-    class extends HTMLElement implements VEditorElement {
-      private editor: HTMLElement;
-      private headerCallbacks: {
-        target: HTMLElement;
-        callback: (event: MouseEvent) => unknown;
-      }[] = [];
-
-      constructor() {
-        super();
-
-        const style = document.createElement('style');
-        style.innerHTML = [
-          ':host { background: #9393933d; width: 100%; display: block; }',
-          ':host > div { display: grid; width: 100%; height: 100%; }',
-          ':host > div > div { outline: none; overflow: auto; }',
-        ].join('');
-
-        this.editor = document.createElement('div');
-        if (!this.readonly) {
-          this.editor.contentEditable = 'true';
-        }
-        this.editor.addEventListener('paste', (event) => {
-          event.preventDefault();
-          event.stopPropagation();
-          console.log(event);
-          // Non-standard API: https://caniuse.com/mdn-api_shadowroot_getselection
-          const selection = (<any>shadow).getSelection(); //document.getSelection();
-          if (event.clipboardData && selection) {
-            this.onPaste(event.clipboardData, selection);
-          }
-        });
-
-        const contents = document.createElement('div');
-        contents.appendChild(this.editor);
-
-        const shadow = this.attachShadow({ mode: 'open' });
-        shadow.appendChild(style);
-        shadow.appendChild(contents);
-
-        this.changeHeader();
-      }
-
-      get readonly() {
-        return this.hasAttribute('readonly');
-      }
-
-      set readonly(value: boolean) {
-        if (value) {
-          this.editor.contentEditable = 'false';
-          this.setAttribute('readonly', '');
-        } else {
-          this.editor.contentEditable = 'true';
-          this.removeAttribute('readonly');
-        }
-      }
-
-      protected changeHeader() {
-        for (const item of this.headerCallbacks) {
-          item.target.removeEventListener('click', item.callback);
-        }
-        this.headerCallbacks = [];
-        const query = this.getAttribute('header');
-        if (!query) {
-          return;
-        }
-        const callback = (event: MouseEvent) => {
-          const target = <HTMLElement> event.target;
-          console.log(target);
-          const selection = document.getSelection();
-          console.log(selection);
-        };
-        const elements = document.querySelectorAll(query);
-        for (const element of elements) {
-          (<HTMLElement> element).addEventListener('click', callback);
-          this.headerCallbacks.push({
-            target: <HTMLElement> element,
-            callback: callback,
-          });
-        }
-      }
-
-      protected onPaste(data: DataTransfer, selection: Selection) {
+})(
+  <HTMLScriptElement> document.currentScript,
+  (script: HTMLScriptElement, tagname: string) => {
+    class DefaultVEditorEvent implements VEditorEvent {
+      onPaste(
+        editor: VEditorElement,
+        data: DataTransfer,
+        selection: Selection,
+      ) {
         const html = data.getData('text/html');
         const parent = document.createElement('div');
         if (html) {
@@ -105,39 +41,112 @@ interface VEditorElement extends HTMLElement {
         } else {
           parent.textContent = data.getData('text/plain');
         }
-
-        const range = selection.getRangeAt(0);
-        range.deleteContents();
-        for (const element of parent.childNodes) {
-          range.insertNode(element);
-        }
-        selection.collapseToEnd();
+        editor.insertNodes(parent.childNodes, selection);
       }
+    }
 
-      static get observedAttributes() {
-        return ['readonly', 'header'];
-      }
+    customElements.define(
+      tagname,
+      class extends HTMLElement implements VEditorElement {
+        public editorEvent: VEditorEvent = new DefaultVEditorEvent();
 
-      attributeChangedCallback(
-        name: string,
-        oldValue: string,
-        newValue: string,
-      ) {
-        if (oldValue === newValue) {
-          return;
+        private headerCallbacks: {
+          target: HTMLElement;
+          callback: (event: MouseEvent) => unknown;
+        }[] = [];
+
+        constructor() {
+          super();
+
+          const style = document.createElement('style');
+          style.innerHTML = [
+            ':host { background: #9393933d; width: 100%; display: block; overflow: auto; padding: 8px; border-radius: 4px; box-sizing: border-box; resize: both; outline: none; }',
+          ].join('');
+
+          this.addEventListener('paste', (event) => {
+            this.onPasteEvent(event);
+          });
+
+          const shadow = this.attachShadow({ mode: 'open' });
+          shadow.appendChild(style);
+          shadow.appendChild(document.createElement('slot'));
+
+          this.changeHeader();
         }
-        switch (name) {
-          case 'readonly': {
-            this.readonly = newValue !== null || newValue === 'true';
+
+        protected changeHeader() {
+          // Reset all header callbacks.
+          for (const item of this.headerCallbacks) {
+            item.target.removeEventListener('click', item.callback);
+          }
+          this.headerCallbacks = [];
+          const selector = this.getAttribute('header');
+          if (!selector) {
             return;
           }
-          case 'header': {
-            this.changeHeader();
-            return;
+
+          const callback = (event: MouseEvent) => {
+            const target = <HTMLElement> event.target;
+            console.log(target);
+            const selection = document.getSelection();
+            console.log(selection);
+          };
+
+          for (const element of document.querySelectorAll(selector)) {
+            (<HTMLElement> element).addEventListener('click', callback);
+            this.headerCallbacks.push({
+              target: <HTMLElement> element,
+              callback: callback,
+            });
           }
         }
-      }
-    },
-    script.dataset.tagname,
-  );
-});
+
+        public insertNodes(
+          nodes: NodeListOf<ChildNode> | ChildNode[],
+          selection: Selection,
+        ) {
+          const range = selection.getRangeAt(0);
+          range.deleteContents();
+          for (const element of nodes) {
+            range.insertNode(element);
+          }
+          selection.collapseToEnd();
+        }
+
+        protected onPasteEvent(event: ClipboardEvent) {
+          event.preventDefault();
+          event.stopPropagation();
+          const data = event.clipboardData;
+          if (!data) {
+            return;
+          }
+          const selection = document.getSelection();
+          if (!selection) {
+            return;
+          }
+          this.editorEvent.onPaste(this, data, selection);
+        }
+
+        static get observedAttributes() {
+          return ['header'];
+        }
+
+        attributeChangedCallback(
+          name: string,
+          oldValue: string,
+          newValue: string,
+        ) {
+          if (oldValue === newValue) {
+            return;
+          }
+          switch (name) {
+            case 'header': {
+              this.changeHeader();
+              return;
+            }
+          }
+        }
+      },
+    );
+  },
+);
